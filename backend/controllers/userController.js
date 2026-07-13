62b8c68df69c67ab54ae853ad77839228a5ede87
Rayan Halder <merayanhalder06@gmail.com>
2026-07-13 19:30:36 +0530

    separated into routes and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,69 @@
+const pool = require("../config/db");
+
+const createUser=async(req, res)=>{
+    try {
+        const {first_name, last_name, gender, dob, email}=req.body;
+        const newUser=await pool.query("INSERT INTO users (first_name, last_name, gender, date_of_birth, email) VALUES ($1, $2, $3, $4, $5)", [first_name, last_name, gender, dob, email])
+        res.json(newUser.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const getAllUsers=async(req, res)=>{
+    try {
+        const allUsers=await pool.query("SELECT * FROM users");
+        res.json(allUsers.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const getUserById=async(req, res)=>{
+    try {
+        const {id}=req.params;
+        const user=await pool.query("SELECT * FROM users WHERE id=$1", [id])
+        res.json(user.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const updateUser=async(req, res)=>{
+    try {
+        const {id}=req.params;
+        const updates=req.body;
+
+        const cols=['first_name', 'last_name', 'gender', 'date_of_birth', 'email'];
+
+        const keys=Object.keys(updates).filter(key=>cols.includes(key));
+
+        if(keys.length===0){
+            return res.json({error: "No valid fields provided"});
+        }
+
+        const setClause=keys.map((key, index)=> `"${key}" = $${index + 1}`).join(", ");
+
+        const queryValues=keys.map(key=>updates[key]);
+        queryValues.push(id);
+
+        const updatedUser=await pool.query(`UPDATE users SET ${setClause} WHERE id = $${queryValues.length} RETURNING *;`, 
+            queryValues);
+
+        res.json(updatedUser.rows[0]);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const deleteUser=async(req, res)=>{
+    try {
+        const {id}=req.params;
+        const deletedUser=await pool.query("DELETE FROM users WHERE id=$1", [id]);
+        res.json("user deleted");
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports={createUser, getAllUsers, getUserById, updateUser, deleteUser};
--- backend/index.js
+++ backend/index.js
@@ -2,82 +2,86 @@ const express=require("express");
 const cors=require("cors");
 require('dotenv').config();
 const pool = require("./config/db");
+const userRoutes=require("./routes/userRoutes");
+
 
 const app=express();
 
 app.use(cors());
 app.use(express.json());
 
-//create user
-app.post("/user", async(req, res)=>{
-    try {
-        const {first_name, last_name, gender, dob, email}=req.body;
-        const newUser=await pool.query("INSERT INTO users (first_name, last_name, gender, date_of_birth, email) VALUES ($1, $2, $3, $4, $5)", [first_name, last_name, gender, dob, email]);
-        res.json(newUser.rows[0]);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+app.use('/user', userRoutes);
 
-//get all users
-app.get("/user", async(req, res)=>{
-    try {
-        const allUsers=await pool.query("SELECT * FROM users");
-        res.json(allUsers.rows);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+// //create user
+// app.post("/user", async(req, res)=>{
+//     try {
+//         const {first_name, last_name, gender, dob, email}=req.body;
+//         const newUser=await pool.query("INSERT INTO users (first_name, last_name, gender, date_of_birth, email) VALUES ($1, $2, $3, $4, $5)", [first_name, last_name, gender, dob, email]);
+//         res.json(newUser.rows[0]);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// })
 
-//get a user by id
-app.get("/user/:id", async(req, res)=>{
-    try {
-        const {id}=req.params;
-        const user=await pool.query("SELECT * FROM users WHERE id=$1", [id])
-        res.json(user.rows);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+// //get all users
+// app.get("/user", async(req, res)=>{
+//     try {
+//         const allUsers=await pool.query("SELECT * FROM users");
+//         res.json(allUsers.rows);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// })
 
-//update user
-app.patch("/user/:id", async(req, res)=>{
-    try {
-        const {id}=req.params;
-        const updates=req.body;
+// //get a user by id
+// app.get("/user/:id", async(req, res)=>{
+//     try {
+//         const {id}=req.params;
+//         const user=await pool.query("SELECT * FROM users WHERE id=$1", [id])
+//         res.json(user.rows);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// })
 
-        const cols=['first_name', 'last_name', 'gender', 'date_of_birth', 'email'];
+// //update user
+// app.patch("/user/:id", async(req, res)=>{
+//     try {
+//         const {id}=req.params;
+//         const updates=req.body;
 
-        const keys=Object.keys(updates).filter(key=>cols.includes(key));
+//         const cols=['first_name', 'last_name', 'gender', 'date_of_birth', 'email'];
 
-        if(keys.length===0){
-            return res.json({error: "No valid fields provided"});
-        }
+//         const keys=Object.keys(updates).filter(key=>cols.includes(key));
 
-        const setClause=keys.map((key, index)=> `"${key}" = $${index + 1}`).join(", ");
+//         if(keys.length===0){
+//             return res.json({error: "No valid fields provided"});
+//         }
 
-        const queryValues=keys.map(key=>updates[key]);
-        queryValues.push(id);
+//         const setClause=keys.map((key, index)=> `"${key}" = $${index + 1}`).join(", ");
 
-        const updatedUser=await pool.query(`UPDATE users SET ${setClause} WHERE id = $${queryValues.length} RETURNING *;`, 
-            queryValues);
+//         const queryValues=keys.map(key=>updates[key]);
+//         queryValues.push(id);
 
-        res.json(updatedUser.rows[0]);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+//         const updatedUser=await pool.query(`UPDATE users SET ${setClause} WHERE id = $${queryValues.length} RETURNING *;`, 
+//             queryValues);
 
-//delete user
-app.delete("/user/:id", async(req, res)=>{
-    try {
-        const {id}=req.params;
-        const deletedUser=await pool.query("DELETE FROM users WHERE id=$1", [id]);
-        res.json("user deleted");
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+//         res.json(updatedUser.rows[0]);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// })
+
+// //delete user
+// app.delete("/user/:id", async(req, res)=>{
+//     try {
+//         const {id}=req.params;
+//         const deletedUser=await pool.query("DELETE FROM users WHERE id=$1", [id]);
+//         res.json("user deleted");
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// })
 
 app.listen(5000, ()=>{
     console.log("server running on port 5000");
