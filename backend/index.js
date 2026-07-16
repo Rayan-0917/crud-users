fcfa9de6aee74cb0f452be258d3c283dce0d731e
Rayan Halder <merayanhalder06@gmail.com>
2026-07-16 18:22:39 +0530

    connecting to supabase

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ const userRoutes=require("./routes/userRoutes");
 
 const app=express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+}));
 app.use(express.json());
 
 app.use('/user', userRoutes);
